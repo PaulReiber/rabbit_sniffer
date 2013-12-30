a975accd7d8cc8f1c0f6f73da4a6c660c47079e6
Victor Denisov <vdenisov@mirantis.com>
2013-12-30 00:21:13 -0800

    Use argument count instead of all arguments shell variable

--- run_sniffer.sh
+++ run_sniffer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ $@ -ne 2 ]]; then
+if [[ $# -ne 1 ]]; then
     echo "Usage: run_sniffer.sh [routing_key]"
     exit 1
 fi
 
 sudo rabbitmqctl trace_on
-./rabbit_sniffer.py $1
+./rabbit_sniffer.py "$1"
 sudo rabbitmqctl trace_off
